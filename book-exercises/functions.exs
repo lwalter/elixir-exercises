4a829618dbe5a7b60f527568f07151783a52cb6e
Lucas Walter <lwalter1022@hotmail.com>
2016-12-17 21:06:26 -0800

    Wrapping up functions chapter

--- book-exercises/functions.exs
+++ book-exercises/functions.exs
@@ -2,7 +2,7 @@ fizzBuzz = fn
   0, 0, _ -> IO.puts("FizzBuzz")
   0, _, _ -> IO.puts("Fizz")
   _, 0, _ -> IO.puts("Buzz")
-  arg1, arg2, arg3 -> IO.puts(arg3)
+  _, _, arg3 -> IO.puts(arg3)
 end
 
 doFizzBuzz = fn(n) ->
@@ -16,3 +16,17 @@ doFizzBuzz.(13)
 doFizzBuzz.(14)
 doFizzBuzz.(15)
 doFizzBuzz.(16)
+
+prefix = fn(prefix) ->
+   fn(suffix) ->
+     "#{prefix} #{suffix}"
+   end
+ end
+
+ mrs = prefix.("Mrs")
+ IO.puts(mrs.("Smith"))
+ IO.puts(prefix.("Elixir").("Rocks"))
+
+
+ Enum.map([1, 2, 3, 4], &(&1 + 2))
+ Enum.map([1, 2, 3, 4], &(IO.inspect(&1)))

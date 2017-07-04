ff9bceccafcc28a0a421a73abd07179877c11463
Lucas Walter <lwalter1022@hotmail.com>
2017-07-04 14:43:20 -0700

    fixed import error for unknown method

--- book-exercises/ch13/issues/test/cli_test.exs
+++ book-exercises/ch13/issues/test/cli_test.exs
@@ -1,7 +1,7 @@
 defmodule CliTest do
     use ExUnit.Case
 
-    import Issues.CLI, only: [ parse_args: 1, sort_ascending_by_created_at: 1, convert_to_list_of_hashdicts: 1 ]
+    import Issues.CLI, only: [ parse_args: 1, sort_ascending_by_created_at: 1, convert_to_list_of_maps: 1 ]
 
     test ":help returned by option parsing with -h and --help options" do
         assert parse_args(["-h", "anything"]) == :help
